5d4b92e5e6e944709f025e63ac47f68417313eaa
arktal <101146331+gpollet@users.noreply.github.com>
2023-01-10 10:45:24 +0100

    Added error page

--- src/components/Thumb.jsx
+++ src/components/Thumb.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 
 const Thumb = ({ logement }) => {
   return (
-    <Link to={logement.id} className="thumb" >
+    <Link to={"logements/"+logement.id} className="thumb" >
       <img src={logement.cover} alt={logement.title} />
       <figure>{logement.title}</figure>
     </Link>
--- src/pages/Logement.jsx
+++ src/pages/Logement.jsx
@@ -1,41 +1,44 @@
 import React from "react"
-import { useLocation } from "react-router-dom"
+import { useParams } from "react-router-dom"
 import logements from "../api/logements.json"
 import Carousel from "../components/Carousel"
 import Dropdown from "../components/Dropdown"
 import Tag from "../components/Tag"
+import Error from "./Error"
 
 const Logement = () => {
   // Retrieves data for the current home based on the url
-  let location = useLocation()
-  const logementData = logements.find((logement) => "/" + logement.id === location.pathname)
-  //console.log(logementData)
 
-  const logementTags = logementData.tags.map((tag) => <Tag tagName={tag} key={tag} />)
+  const { logementId } = useParams()
+  const logementData = logements.find((logement) => logement.id === logementId)
+
+  const logementTags = () => logementData.tags.map((tag) => <Tag tagName={tag} key={tag} />)
 
   // Displays the rating
-  const logementRatingStars = Array.from({ length: logementData.rating }, (_, i) => {
-    return (
-      <img
-        className="rating-stars"
-        src="/images/icons/star-red.png"
-        alt="Filled star"
-        key={"red-star" + i}
-      />
-    )
-  })
-  const logementRatingToFive = Array.from({ length: 5 - logementData.rating }, (_, i) => {
-    return (
-      <img
-        className="rating-stars"
-        src="/images/icons/star-empty.png"
-        alt="Empty star"
-        key={"empty-star" + i}
-      />
-    )
-  })
+  const logementRatingStars = () =>
+    Array.from({ length: logementData.rating }, (_, i) => {
+      return (
+        <img
+          className="rating-stars"
+          src="/images/icons/star-red.png"
+          alt="Filled star"
+          key={"red-star" + i}
+        />
+      )
+    })
+  const logementRatingToFive = () =>
+    Array.from({ length: 5 - logementData.rating }, (_, i) => {
+      return (
+        <img
+          className="rating-stars"
+          src="/images/icons/star-empty.png"
+          alt="Empty star"
+          key={"empty-star" + i}
+        />
+      )
+    })
 
-  const createEquipmentList = (
+  const createEquipmentList = () => (
     <ul>
       {logementData.equipments.map((equipment) => (
         <li key={equipment}>{equipment}</li>
@@ -45,35 +48,44 @@ const Logement = () => {
 
   return (
     <>
-      <Carousel carouselData={logementData} />
+    {/* If there is data, display the page. Otherwise, display error page */}
+      {logementData ? (
+        <>
+          <Carousel carouselData={logementData} />
 
-      <div className="logement-top-info">
-        <div>
-          <h1>{logementData.title}</h1>
-          <h2>{logementData.location}</h2>
-        </div>
-        <div className="host-presentation">
-          <p>{logementData.host.name}</p>
-          <img
-            className="host-portrait"
-            src={logementData.host.picture}
-            alt={"Photo de " + logementData.host.name}
-          />
-        </div>
-      </div>
+          <div className="logement-top-info">
+            <div>
+              <h1>{logementData.title}</h1>
+              <h2>{logementData.location}</h2>
+            </div>
+            <div className="host-presentation">
+              <p>{logementData.host.name}</p>
+              <img
+                className="host-portrait"
+                src={logementData.host.picture}
+                alt={"Photo de " + logementData.host.name}
+              />
+            </div>
+          </div>
 
-      <div className="tags-and-rating">
-        <div className="tagList">{logementTags}</div>
-        <div className="host-rating">
-          {logementRatingStars}
-          {logementRatingToFive}
-        </div>
-      </div>
+          <div className="tags-and-rating">
+            <div className="tagList">{logementTags()}</div>
+            <div className="host-rating">
+              {logementRatingStars()}
+              {logementRatingToFive()}
+            </div>
+          </div>
 
-      <div className="logement-dropdown">
-        <Dropdown title="Description" content={<p>{logementData.description}</p>} />
-        <Dropdown title="Équipements" content={createEquipmentList} />
-      </div>
+          <div className="logement-dropdown">
+            <Dropdown title="Description" content={<p>{logementData.description}</p>} />
+            <Dropdown title="Équipements" content={createEquipmentList()} />
+          </div>
+        </>
+      ) : (
+        <>
+          <Error />
+        </>
+      )}
     </>
   )
 }
