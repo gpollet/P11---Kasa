45ade8749836f9b8056456b0b8ee353aa34f2a82
arktal <101146331+gpollet@users.noreply.github.com>
2023-01-10 11:03:23 +0100

    Added About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,39 @@
-import React from 'react';
-import SectionTopBanner from '../components/SectionTopBanner';
+import React from "react"
+import SectionTopBanner from "../components/SectionTopBanner"
+import Dropdown from "../components/Dropdown"
 
 const About = () => {
+  const dropdownsContent = [
+    {
+      title: "Fiabilité",
+      content:
+        "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.",
+    },
+    {
+      title: "Respect",
+      content:
+        "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.",
+    },
+    {
+      title: "Service",
+      content:
+        "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.",
+    },
+    {
+      title: "Sécurité",
+      content:
+        "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
+    },
+  ]
+  const createAboutDropdowns = dropdownsContent.map((dropdown) => (
+    <Dropdown title={dropdown.title} content={dropdown.content} key={dropdown.title} />
+  ))
   return (
-    <div>
+    <>
       <SectionTopBanner />
-      {/*<p>A propos</p>*/}
-    </div>
-  );
-};
+      <div className="about-dropdowns">{createAboutDropdowns}</div>
+    </>
+  )
+}
 
-export default About;
+export default About
